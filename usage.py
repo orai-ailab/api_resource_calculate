3a32692446fa39fb6ff85ba3c4b5e7e66c9249c7
KhangPTT373 <phmtrntuankhang@gmail.com>
2024-08-21 09:58:11 +0700

    update endpoints

--- usage.py
+++ usage.py
@@ -1,138 +1,41 @@
 import requests
-from utils import  get_start_end
+from utils import get_start_end
 import query
 from prometheus_api_client import PrometheusConnect
 
 prom = PrometheusConnect(url='https://prom-gpu.orai.network/', disable_ssl=True)
 
-
-
-def func_query_RAM_requested(window,aggregate,query=query.queryFmtRAMRequests,step=None):
-    start,end = get_start_end(window)
-    step = step if step else end-start
-    response = prom.custom_query(query=query%(window,aggregate))
-
+def fetch_metric_data(window, aggregate, query_template, metric_key, step=None):
+    start, end = get_start_end(window)
+    step = step if step else end - start
+    response = prom.custom_query(query=query_template % (window, aggregate))
+    
     result = {}
     for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['ram_alloc'] = float(d['value'][1])
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['ram_alloc'] = float(d['value'][1])
-
-            result[agg_factor] =  [
-                resource
-            ]
+        agg_factor = d['metric'].get(aggregate)
+        if agg_factor not in result:
+            result[agg_factor] = []
+            
+        resource = d['metric']
+        resource[metric_key] = float(d['value'][1])
+        result[agg_factor].append(resource)
+    
     return result
 
+def func_query_RAM_requested(window, aggregate, step=None):
+    return fetch_metric_data(window, aggregate, query.queryFmtRAMRequests, 'ram_alloc', step)
 
-def func_query_RAM_usage(window,aggregate,query=query.queryFmtRAMUsageAvg,step=None):
-    start,end = get_start_end(window)
-    step = step if step else end-start
-    response = prom.custom_query(query=query%(window,aggregate))
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['ram_usage'] = float(d['value'][1])
+def func_query_RAM_usage(window, aggregate, step=None):
+    return fetch_metric_data(window, aggregate, query.queryFmtRAMUsageAvg, 'ram_usage', step)
 
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['ram_usage'] = float(d['value'][1])
+def func_query_CPU_request(window, aggregate, step=None):
+    return fetch_metric_data(window, aggregate, query.queryFmtCPURequests, 'cpu_requested', step)
 
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
+def func_query_CPU_usage(window, aggregate, step=None):
+    return fetch_metric_data(window, aggregate, query.queryFmtCPUUsageAvg, 'cpu_usage', step)
 
+def func_query_GPUs_request(window, aggregate, step=None):
+    return fetch_metric_data(window, aggregate, query.queryFmtGPUsRequested, 'gpu_usage', step)
 
-def func_query_CPU_request(window,aggregate,query=query.queryFmtCPURequests,step=None):
-    start,end = get_start_end(window)
-    step = step if step else end-start
-    response = prom.custom_query(query=query%(window,aggregate))
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['cpu_requested'] = float(d['value'][1])
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['cpu_requested'] = float(d['value'][1])
-
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
-
-def func_query_CPU_usage(window,aggregate,query=query.queryFmtCPUUsageAvg,step=None):
-    start,end = get_start_end(window)
-    step = step if step else end-start
-    response = prom.custom_query(query=query%(window,aggregate))
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['cpu_usage'] = float(d['value'][1])
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['cpu_usage'] = float(d['value'][1])
-
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
-
-
-def func_query_GPUs_request(window,aggregate,query=query.queryFmtGPUsRequested,step=None):
-    start,end = get_start_end(window)
-    step = step if step else end-start
-    response = prom.custom_query(query=query%(window,aggregate))
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['gpu_usage'] = float(d['value'][1])
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['gpu_usage'] = float(d['value'][1])
-
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
-
-def func_query_GPUs_usage(window,aggregate,query=query.queryFmtGPUsUsageAvg,step=None):
-    start,end = get_start_end(window)
-    step = step if step else end-start
-    response = prom.custom_query(query=query%(window,aggregate))
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['gpu_usage'] = float(d['value'][1])
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['gpu_usage'] = float(d['value'][1])
-
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
+def func_query_GPUs_usage(window, aggregate, step=None):
+    return fetch_metric_data(window, aggregate, query.queryFmtGPUsUsageAvg, 'gpu_usage', step)
--- usage_range.py
+++ usage_range.py
@@ -1,126 +1,40 @@
 import requests
-from utils import  get_start_end
+from utils import get_start_end
 import query
 from prometheus_api_client import PrometheusConnect
 
 prom = PrometheusConnect(url='https://prom-gpu.orai.network/', disable_ssl=True)
 
-def func_RAM_request_chart(window,aggregate,resolution,query=query.queryFmtRAMRequests_chart):
-    start,end = get_start_end(window)
-    response = prom.custom_query_range(query=query%(window),start_time=start, end_time=end, step=resolution)
+def fetch_chart_data(window, aggregate, resolution, query, metric_key):
+    start, end = get_start_end(window)
+    response = prom.custom_query_range(query=query % window, start_time=start, end_time=end, step=resolution)
+    
     result = {}
     for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['ram_request_chart'] = d['values']
+        agg_factor = d['metric'].get(aggregate)
+        if agg_factor not in result:
+            result[agg_factor] = []
 
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['ram_request_chart'] = d['values']
-
-            result[agg_factor] =  [
-                resource
-            ]
+        resource = d['metric']
+        resource[metric_key] = d['values']
+        result[agg_factor].append(resource)
+    
     return result
 
-def func_RAM_usage_chart(window,aggregate,resolution,query=query.queryFmtRAMUsageAvg_chart):
-    start,end = get_start_end(window)
-    response = prom.custom_query_range(query=query%(window),start_time=start, end_time=end, step=resolution)
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['ram_usage_chart'] = d['values']
+def func_RAM_request_chart(window, aggregate, resolution):
+    return fetch_chart_data(window, aggregate, resolution, query.queryFmtRAMRequests_chart, 'ram_request_chart')
 
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['ram_usage_chart'] = d['values']
+def func_RAM_usage_chart(window, aggregate, resolution):
+    return fetch_chart_data(window, aggregate, resolution, query.queryFmtRAMUsageAvg_chart, 'ram_usage_chart')
 
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
+def func_CPU_request_chart(window, aggregate, resolution):
+    return fetch_chart_data(window, aggregate, resolution, query.queryFmtCPURequests_chart, 'cpu_request_chart')
 
-def func_CPU_request_chart(window,aggregate,resolution,query=query.queryFmtCPURequests_chart):
-    start,end = get_start_end(window)
-    response = prom.custom_query_range(query=query%(window),start_time=start, end_time=end, step=resolution)
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['cpu_request_chart'] = d['values']
+def func_CPU_usage_chart(window, aggregate, resolution):
+    return fetch_chart_data(window, aggregate, resolution, query.queryFmtCPUUsageAvg_chart, 'cpu_usage')
 
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['cpu_request_chart'] = d['values']
+def func_GPUs_request_chart(window, aggregate, resolution):
+    return fetch_chart_data(window, aggregate, resolution, query.queryFmtGPUsRequested_chart, 'gpu_request_chart')
 
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
-
-def func_CPU_usage_chart(window,aggregate,resolution,query=query.queryFmtCPUUsageAvg_chart):
-    start,end = get_start_end(window)
-    response = prom.custom_query_range(query=query%(window),start_time=start, end_time=end, step=resolution)
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['cpu_usage'] = d['values']
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['cpu_usage'] = d['values']
-
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
-
-def func_GPUs_request_chart(window,aggregate,resolution,query=query.queryFmtGPUsRequested_chart):
-    start,end = get_start_end(window)
-    response = prom.custom_query_range(query=query%(window),start_time=start, end_time=end, step=resolution)
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['gpu_request_chart'] = d['values']
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['gpu_reuqest_chart'] = d['values']
-
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
-
-def func_GPUs_usage_chart(window,aggregate,resolution,query=query.queryFmtGPUsUsageAvg_chart):
-    start,end = get_start_end(window)
-    response = prom.custom_query_range(query=query%(window),start_time=start, end_time=end, step=resolution)
-    result = {}
-    for d in response:
-        agg_factor = d['metric'][f'{aggregate}']
-        if agg_factor in result:
-            resource = d['metric']
-            resource['gpu_usage_chart'] = d['values']
-
-            result[agg_factor].append(resource)
-        else:
-            resource = d['metric']
-            resource['gpu_usage_chart'] = d['values']
-
-            result[agg_factor] =  [
-                resource
-            ]
-    return result
+def func_GPUs_usage_chart(window, aggregate, resolution):
+    return fetch_chart_data(window, aggregate, resolution, query.queryFmtGPUsUsageAvg_chart, 'gpu_usage_chart')
